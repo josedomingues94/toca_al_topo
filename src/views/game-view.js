51bd006598c2bfb9be2ea41c9bd1134fba226f36
josedomingues94 <josedomingues2894@gmail.com>
2023-10-02 19:49:59 +0200

    feat: add functionality

--- src/views/game-view.js
+++ src/views/game-view.js
@@ -54,15 +54,15 @@ export class Game extends LitElement {
     name: { type: String },
     puntos: { type: Number},
     mensajeAcierto: {type: String, value: ""},
-    dificultad: {type: String, value: ""}
+    dificultad: {type: String}
   };
 
   constructor() {
     super();
     this.name = "START";
     this.puntos = 0;
+    this.dificultad = "";
     this.image = html`<img class="img" src="../../assets/images/topo.png">`;
-    this.user = null;
     this.square = [html`<div class="square"></div>`, html`<div class="square"></div>`, html`<div class="square"></div>`,
                   html`<div class="square"></div>`, html`<div class="square"></div>`, html`<div class="square"></div>`,
                   html`<div class="square"></div>`, html`<div class="square"></div>`, html`<div class="square"></div>`];
@@ -75,38 +75,62 @@ export class Game extends LitElement {
           <card-lit></card-lit>
         </div>
         <div class="right">
-          <label>Nivel</label>
+          <label>Nivel</label>   
           <combo-lit @item-selected="${this.handleSelectedOption}"
-            id="dificultad"
+          id="dificultad"
             .options="${[
               { value: "option1", label: "Bajo" },
               { value: "option2", label: "Medio" },
               { value: "option3", label: "Alto" },
             ]}"
-          >
-          
-          </combo-lit>
-          
+          >   
+          </combo-lit>  
         </div>
       </header>
       <h3>Puntos: <span>${this.puntos}</span></h3>
       <div class="grid">
-        ${this.square.map(item => html`<hitmole-lit .enableButton="${this.clickDisabled}"></hitmole-lit>`)}
+        ${this.square.map(item => html`<hitmole-lit .enableButton="${this.clickDisabled}" .item=${item} @click="${this._seleccionaCuadro(item)}"></hitmole-lit>`)}
       </div>
       <boton-lit @click=${this.startStop} name="${this.name}"></boton-lit>
     `;
   }
 
   handleSelectedOption(event){
-    this.dificultad = event.detail;
+    this.options = event.detail;
+    console.log(this.options);
+  }
+
+  movertopo(){
+    let movRandom;
+    
+
+    setTimeout(() =>{
+      movRandom = Math.floor(Math.random() * this.square.length);
+      console.log(movRandom);
+    }, 1000);
+
+    setTimeout(() =>{
+      movRandom = Math.floor(Math.random() * this.square.length);
+      console.log(movRandom);
+    }, 750);
+
+    setTimeout(() =>{
+      movRandom = Math.floor(Math.random() * this.square.length);
+      console.log(movRandom);
+    }, 500);
     
   }
 
- 
+  _seleccionaCuadro(){
+    if(!this.clickDisabled){
+      this.clickDisabled = true;
+    }
+  }
+
+
   startStop() {
     this.name = this.name === "START" ? "STOP" : "START";
     console.log(this.square);
-    
   }
 }
 customElements.define("game-view", Game);
